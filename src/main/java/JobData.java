3fc76a7a8febac7d3325d13720750926d5e7b3c8
Zahon Matthews <zaematthews@gmail.com>
2022-08-06 22:13:42 -0400

    need to call findByValue

--- src/main/java/JobData.java
+++ src/main/java/JobData.java
@@ -99,7 +99,15 @@ public class JobData {
         loadData();
 
         // TODO - implement this method
-        return null;
+        ArrayList <HashMap<String, String>> jobs = new ArrayList<>();
+        for (HashMap<String, String> row: allJobs){
+            for (HashMap.Entry<String, String> job: row.entrySet()){
+                if (job.getValue().toUpperCase().contains(value) && !jobs.contains(row)){
+                    jobs.add(row);
+                }
+            }
+        }
+        return jobs;
     }
 
     /**
